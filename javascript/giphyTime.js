d1f861dad70e337a3abdae4b242f56394aa50a27
parrn1 <mrnickp@comcast.net>
2019-04-09 13:01:38 -0400

    javascript should be finished can add buttons which add the gifs

--- javascript/giphyTime.js
+++ javascript/giphyTime.js
@@ -1,10 +1,45 @@
-$("#search").on("click", function(event) {
-  
+var btns = ["zoidberg", "skelletor", "pepe", "warhammer"];
+
+
+
+$("#search").on("click", function (event) {
+
   event.preventDefault();
 
   var $userSearch = $("#userSearch").val();
 
   console.log($userSearch);
+
+  btns.push($userSearch);
+
+  //call function that makes new button appear
+  createBtns();
+
+})
+
+function createBtns() {
+
+  $("#btnLand").empty(); //empty btnLand
+
+  for (var i = 0; i < btns.length; i++) { //need to add bootstrap !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    var newBtn = $("<button>");
+    newBtn.text(btns[i]);
+    newBtn.attr("class", "gifBtn");
+    $("#btnLand").prepend(newBtn);
+
+  }
+
+}
+
+//when you click a button display relevant gifs
+$("#btnLand").on("click", ".gifBtn", function (event) {
+
+  console.log(this);
+
+  var $userSearch = $(this).text();
+
+  $("#gifLand").empty();
 
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=4NgZUEznrfAEcVoDyY79HQRZsB89CWm4&limit=10&q=" + $userSearch;
 
@@ -23,16 +58,39 @@ $("#search").on("click", function(event) {
         var $gif = $("<img>");
         var stillUrl = result[i].images.fixed_height_still.url;
         var gifUrl = result[i].images.fixed_height.url;
-        
+        var rating = result[i].rating;
+
         $gif.attr("src", stillUrl);
         $gif.attr("stillSrc", stillUrl);
         $gif.attr("gifSrc", gifUrl);
-        
-        $("#gifLand").append($gif);
+        $gif.attr("id", "gif");
+        $gif.attr("class", "stillGif");
+
+        $("#gifLand").prepend($gif);
+        $("#gifLand").prepend(`<span>${rating}</span>`);
 
       }
 
     });
 
+})
+
+$("#gifLand").on("click", "#gif", function () { //when we click on a gif it either pauses or starts
+
+  //if still make moving || if moving make still
+
+  if ($(this).attr("class") == "stillGif") {
+
+    $(this).attr("class", "gifGif");
+    $(this).attr("src", $(this).attr("gifSrc"));
+
+  } else if ($(this).attr("class") == "gifGif") {
+
+    $(this).attr("class", "stillGif");
+    $(this).attr("src", $(this).attr("stillSrc"));
+
+  }
 
 })
+
+createBtns();
